f144e50f6279108e6f12e87ea2c38665e7c458bc
cem-sucu <cems.cems.17@gmail.com>
2023-06-20 09:25:17 +0200

    mise en place du constructor et de la function ajout compte pour ensemble des compte bancaire du titulaire

--- classes/Titulaire.php
+++ classes/Titulaire.php
@@ -1,7 +1,25 @@
 <?php 
 
 class Titulaire{
-    
+    private string $nom;
+    private string $prenom;
+    private $dateDeNaissance;
+    private string $ville;
+    private $ensembleCompte;
+
+    public function __construct(string $nom,string $prenom, $dateDeNaissance, string $ville, $ensembleCompte )
+    {
+        $this->nom = $nom;
+        $this-> prenom = $prenom;
+        $this-> dateDeNaissance = $dateDeNaissance;
+        $this-> ville = $ville;
+        //ici comme c'est un ensemble de compte, on créé un tableau
+        $this-> ensembleCompte = array();
+    }
+
+    public function ajouterCompte($ensembleCompte){
+        $this-> ensembleCompte = $ensembleCompte;
+    }
 }
 
 
